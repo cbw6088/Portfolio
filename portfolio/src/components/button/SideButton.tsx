1ade8b349ddf20f4b3271a72229f49c067544d2d
cbw6088 <cbw4186@naver.com>
2026-03-10 21:22:52 +0900

    Vercel 배포

---
+++ portfolio/src/components/button/SideButton.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// Backwards-compatible wrapper so old imports using
+// "@/components/button/SideButton" keep working.
+// Internally we just reuse the newer SideButtonTest implementation.
+
+export { default } from "./SideButtonTest";
+
